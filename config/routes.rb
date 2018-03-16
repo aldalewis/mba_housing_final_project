b638ecfe977326b99468eca6f45eb73b9d6f83f9
Raghu Betina <raghu@firstdraft.com>
2018-03-16 09:34:52 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "rentals#index"
   # Routes for the Saved_rental resource:
   # CREATE
