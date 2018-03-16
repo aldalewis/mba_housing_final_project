4752aad55d423bf2572ce7db9ae2e95a2ff29ddd
Raghu Betina <raghu@firstdraft.com>
2018-03-16 09:35:02 +0000

    Whitelist columns in Rental for ActiveAdmin

--- app/admin/rentals.rb
+++ app/admin/rentals.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Rental do
+
+ permit_params :name, :address, :image, :description, :feature, :rent_amount, :date_available, :end_date, :user_id, :roommates
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
